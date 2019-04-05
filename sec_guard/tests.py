7380827dfacfb6393fc81aad56ce630d743d17e4
Rashid9158 <mneyaz96@gmail.com>
2019-04-05 18:23:49 +0530

    removed package resources 0.0 from requirements

--- sec_guard/tests.py
+++ sec_guard/tests.py
@@ -6,7 +6,7 @@ from .models import Package
 #Test Case for model
 class PackageTest(TestCase):
 	def create_package(self, phone="9876543210", orderedfrom="Amazon"):
-		return Package.objects.create(orderedfrom=orderedfrom, productid=productid)
+		return Package.objects.create(orderedfrom=orderedfrom)
 
 	def test_creation(self):
 		w=self.create_package()
